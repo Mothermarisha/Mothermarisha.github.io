08de5e296f7440275ecbfe403886bae964eb43fd
Mothermarisha <135148661+Mothermarisha@users.noreply.github.com>
2024-01-04 19:50:32 -0500

    Update SpeakGoodbye.js

--- Module4-solution/SpeakGoodbye.js
+++ Module4-solution/SpeakGoodbye.js
@@ -1,6 +1,6 @@
 (function (window){
   var speakWord=" Good Bye"
-  var byeSpeaker = function (name) {
+  var byeSpeaker = function(name) {
   console.log (speakWord + "" + name);
   } 
   window.byeSpeaker=byeSpeaker;
